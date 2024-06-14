c38533afc51af138057ea13ff5700ea227c4cb38
Raijohn <166853579+Raijohn@users.noreply.github.com>
2024-06-14 19:04:43 +0200

    FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -12,11 +12,17 @@ if not os.path.isdir(path):
 classafire = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 video = cv2.VideoCapture(0)
-for i in range(100):
+for i in range(30):
     success,frame = video.read()
-    if not success:
-        continue
+    #if not success:
+    #    continue
     GreyFrame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     faces = classafire.detectMultiScale(GreyFrame,1.3,5)
+    for x,y,w,h in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+        currentFrame = GreyFrame[y:y+h,x:x+w]
+        finallImage = cv2.resize(currentFrame,(width,height))
+        cv2.imwrite(path+"\\"+str(i)+".png",finallImage)
     cv2.imshow("frame",frame)
+    cv2.waitKey(10)
     print(faces)
